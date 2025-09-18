bcfd494739a29a2638985bdd9ce4572bc1fe9179
nurherfannyoktari-collab <nurherfannyoktari@gmail.com>
2025-09-18 22:52:22 +0700

    Add files via upload

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -5,37 +5,74 @@ use App\Models\Mahasiswa;
 use GuzzleHttp\Psr7\Request as Psr7Request;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
+use Illuminate\Support\Facades\DB;
+
 
 class MahasiswaController extends Controller
 {
     //create data
-    public function store(Request $request){
-        $mahasiswa = Mahasiswa::create($request->all());
-        return $mahasiswa;
-    }
+public function create () {
+    return view('mahasiswa.create');
+}
+
+public function store(Request $request) {
+
+    $request->validate([
+        'name' => 'required',
+        'email' => 'required',
+        'no_hp' => 'required',
+    ]);
+
+    DB::table('mahasiswas')->insert([
+        'name' => $request->name,
+        'email' => $request->email,
+        'no_hp' => $request->no_hp,
+    ]);
+
+    return redirect()->route('mahasiswa.index');
+}
 
     //read data
-    public function index(){
-        return Mahasiswa::all();
-    }
+public function index()
+{
+    $mahasiswa = DB::table('mahasiswas')->get();
+    return view('mahasiswa.index', compact('mahasiswa'));
+}
 
-    // update data
+
+    // Update data
+public function edit($id)
+{
+    $mahasiswa = DB::table('mahasiswas')->where('id', $id)->first();
+    return view('mahasiswa.edit', compact('mahasiswa'));
+}
+
 public function update(Request $request, $id)
 {
-    $mahasiswa = Mahasiswa::findOrFail($id);
-    $mahasiswa->update($request->all());
+    $request->validate([
+        'name'   => 'required',
+        'email'  => 'required',
+        'no_hp'  => 'required',
+    ]);
 
-    return $mahasiswa;
+    DB::table('mahasiswas')
+        ->where('id', $id)
+        ->update([
+            'name'       => $request->name,
+            'email'      => $request->email,
+            'no_hp'      => $request->no_hp,
+            'updated_at' => now(),
+        ]);
+
+    return redirect()->route('mahasiswa.index');
 }
 
-// delete data
+
+// Menghapus data
 public function destroy($id)
 {
-    $mahasiswa = Mahasiswa::findOrFail($id);
-    $mahasiswa->delete();
-
-    return $mahasiswa;
+    DB::table('mahasiswas')->where('id', $id)->delete();
+    return redirect()->route('mahasiswa.index');
 }
-
 
 }
